4410f4c4194ea3f6a61641fc987bc3b0e531fa5d
Vicky Srivas <v.vicky911@gmail.com>
2021-05-09 02:27:21 +0530

    First Commit

---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/controller/SignUpController.java
@@ -0,0 +1,55 @@
+package com.udacity.jwdnd.course1.cloudstorage.controller;
+
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import com.udacity.jwdnd.course1.cloudstorage.services.HashService;
+import com.udacity.jwdnd.course1.cloudstorage.services.UserService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/signup")
+public class SignUpController {
+
+    private UserService userService;
+
+    public SignUpController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String showSignUpPage(@ModelAttribute("user") User user, Model model){
+        return "signup";
+    }
+
+    @PostMapping
+    public String createNewUserAccount(@ModelAttribute("user") User user, Model model){
+        System.out.println("The username is " + user.getUsername());
+        System.out.println("The password is " + user.getPassword());
+        System.out.println("The firstname is " + user.getFirstname());
+        System.out.println("The lastname is " + user.getLastname());
+
+        String signUpMessage = null;
+        if(userService.checkIfUsernameAlreadyExists(user.getUsername()) != null){
+            signUpMessage = "Username already exists!";
+        }
+
+        if(signUpMessage == null){
+           int rowsAdded = userService.createUser(user);
+           if(rowsAdded == 0){
+                signUpMessage = "There was an error signing you up. Please try again.";
+           }
+        }
+        if(signUpMessage == null){
+            model.addAttribute("SignUpSuccess", true);
+        }
+        else{
+            model.addAttribute("SignUpError", signUpMessage);
+        }
+        return "signup";
+    }
+
+}
---
+++ src/main/java/com/udacity/jwdnd/course1/cloudstorage/services/UserService.java
@@ -0,0 +1,42 @@
+package com.udacity.jwdnd.course1.cloudstorage.services;
+
+import com.udacity.jwdnd.course1.cloudstorage.mapper.UserMapper;
+import com.udacity.jwdnd.course1.cloudstorage.model.User;
+import org.springframework.stereotype.Service;
+
+import java.security.SecureRandom;
+import java.util.Base64;
+
+@Service
+public class UserService {
+
+    private final UserMapper userMapper;
+    private final HashService hashService;
+
+    public UserService(UserMapper userMapper, HashService hashService) {
+        this.userMapper = userMapper;
+        this.hashService = hashService;
+    }
+
+    public User checkIfUsernameAlreadyExists(String username){
+
+        return userMapper.getUsername(username);
+    }
+
+    public int createUser(User user){
+
+        SecureRandom random = new SecureRandom();
+        byte[] salt = new byte[16];
+        random.nextBytes(salt);
+        String encodedSalt = Base64.getEncoder().encodeToString(salt);
+        String hashedPassword = hashService.getHashedValue(user.getPassword(), encodedSalt);
+        System.out.println("The username in Userservice is " + user.getUsername());
+        System.out.println("The password in Userservice  is " + user.getPassword());
+        System.out.println("The firstname in Userservice  is " + user.getFirstname());
+        System.out.println("The lastname in Userservice  is " + user.getLastname());
+
+        return userMapper.addUser(new User(null, user.getUsername(), encodedSalt, hashedPassword, user.getFirstname(), user.getLastname()));
+    }
+
+
+}
